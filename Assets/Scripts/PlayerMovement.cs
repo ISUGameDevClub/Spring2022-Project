f11bb70fdbd4e092fc5296683806f70dfc79c9a6
chairzephyr <alisonweiser4@gmail.com>
2022-02-16 21:47:06 -0600

    day 1--movement stuff
    i am a girl who codes

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	Vector2 direction;
+    public float speed;
+    float Xinput;
+    float Yinput;
+    Rigidbody2D playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Xinput = Input.GetAxis("Horizontal");
+        Yinput = Input.GetAxis("Vertical");
+
+        direction = new Vector2(Xinput, Yinput);
+
+        playerRB.MovePosition((Vector2)transform.position + (direction * speed));
+    }
+}
