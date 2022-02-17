52b1fa8aea3acfb37e4451595b796d03b71b60ca
Disconnectors <57683808+Disconnectors@users.noreply.github.com>
2022-02-16 21:46:05 -0600

    123

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 direction;
+
+    [SerializeField] float speed;
+    float xInput;
+    float yInput;
+    Rigidbody2D playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10;
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        direction = new Vector2(xInput, yInput);
+
+        if (direction.magnitude > 1)
+        {
+            direction.Normalize();
+        }
+
+        playerRB.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+
+    }
+}
