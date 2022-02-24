cf7ab54e7a70c270752138cb7a61075c447ec820
PipotronK <dezigurzero@gmail.com>
2022-02-23 22:03:24 -0600

    made dash movement
    made Dash movement
    disable normal movement while dashing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,23 +8,56 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed;
     float Xinput;
     float Yinput;
+    [SerializeField] float dash = 10;
+    bool dashing = false;
+    [SerializeField] float sec = 0.5f;
+    bool canmove = true;
     Rigidbody2D playerRB;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
     }
+    private void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.LeftShift) && !dashing)
+        {
+            StartCoroutine(DashTime(sec));
+            
+        }
+    }
     private void FixedUpdate()
     {
         Xinput = Input.GetAxis("Horizontal");
         Yinput = Input.GetAxis("Vertical");
-
+        if(canmove)
+        { 
         direction = new Vector2(Xinput, Yinput);
-
+        }
         if (direction.magnitude > 1)
         {
             direction.Normalize();
         }
-        playerRB.MovePosition((Vector2)transform.position + (direction * speed * Time.fixedDeltaTime));
+               
+       
+        if (dashing)
+        {
+            
+            playerRB.MovePosition((Vector2)transform.position + (direction * dash * Time.fixedDeltaTime));
+            
+        }
+        else
+        {
+            playerRB.MovePosition((Vector2)transform.position + (direction * speed * Time.fixedDeltaTime));
+        }
+       
+    }
+    IEnumerator DashTime (float sec)
+    {
+        dashing = true;
+        canmove = false;
+        yield return new WaitForSeconds(sec);
+        canmove = true;
+        dashing = false;
     }
 }
