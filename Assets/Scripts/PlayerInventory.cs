9a1f4d2a4b3eeebcef18baa6b694ad93dd8eccd6
barrettury <barrettury@gmail.com>
2022-03-02 21:38:43 -0600

    Weapon Pick Up
    -Players can pick up weapons by touching them and pressing 'E'
            : It will replace what is in the current slot
            : then it will delete itself
    -Changing the slot with 'Right Shift' swaps what weapon is in the active slot, physically and in the code

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -6,16 +6,37 @@ public class PlayerInventory : MonoBehaviour
 {
   [SerializeField] private Weapon wp1;
   [SerializeField] private Weapon wp2;
+  [SerializeField] private int activeSlot;
 
     // Start is called before the first frame update
     void Start()
     {
-      
+      activeSlot = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    public void ChangeActiveSlot(int s)
+    {
+        activeSlot = s;
+    }
+    public void WeaponSwap(Weapon nW)
+    {
+      if(activeSlot == 1)
+      {
+        wp1 = nW;
+      }
+      else
+      {
+        wp2 = nW;
+      }
+
+    }
+    public int ReturnActiveSlot()
+    {
+      return activeSlot;
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 [System.Serializable]
 public class Weapon
 {
+  public Weapon(string name, float baseDamage,float currDamage, string description)
+  {
+    this.name = name;
+    this.baseDamage = baseDamage;
+    this.currDamage = currDamage;
+    this.description = description;
+  }
 
   public string name;
   public float baseDamage;
